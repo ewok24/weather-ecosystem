76e1561248c9fa68a015557b854983a0217594b3
Daniel Tan <daniel_timothy.tan@mac.com>
2021-08-06 20:15:54 +1000

    nx g remove weather-express-api-weather -d

--- libs/weather-express-api/weather/README.md
+++
@@ -1,7 +0,0 @@
-# weather-express-api-weather
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test weather-express-api-weather` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/weather-express-api/weather/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'weather-express-api-weather',
-  preset: '../../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/weather-express-api/weather',
-};
